24daf0395fbc50cdacf13408ad5a0a55826f5af8
Shay Wacker <shay2442@yahoo.com>
2021-07-20 16:31:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// const cards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names, occasion) {
+    let result = [];
+    for (let i=0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return result;
+}
+
+function countDown(number) {
+    let j = number;
+    while (j >= 0) {
+        console.log(j);
+        j--;
+    }
+}
+
+// countDown();
